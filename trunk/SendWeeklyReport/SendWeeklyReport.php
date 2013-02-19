c5b35b68af68a705321b07bdd271b6e9c639581a
langerheiko@googlemail.com <langerheiko@googlemail.com@cf8d77c8-a08f-8c47-ec4e-e3c4bdea25d2>
2013-02-19 12:29:23 +0000

    Initial SendWeeklyReport.php

---
+++ trunk/SendWeeklyReport/SendWeeklyReport.php
@@ -0,0 +1,24 @@
+<?php
+
+Class SendWeeklyReportPlugin extends MantisPlugin {
+	function register() {
+		$this->name		= 'Send weekly report';
+		$this->description 	= 'Send a weekly report by email to specified users.';
+		$this->page		= 'config';
+
+		$this->version		= '0.1';
+		$this->requires		= array('MantisCore' => '1.2.14');
+		
+		$this->author		= 'eCola GmbH, Heiko Schneider-Lange';
+		$this->contact		= 'hsl@ecola.com';
+		$this->url		= 'http://www.lebensmittel.de';
+	}
+
+	function config() {
+		return array();
+	}
+	
+	function init() {
+		
+	}
+}
